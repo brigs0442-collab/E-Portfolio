6de51e0b7e789766518b48405799c1fc46eedce1
brigs0442-collab <brigs0442@gmail.com>
2025-10-19 09:00:37 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Artificial Intelligence Engineer', 'Web Designer', 'Chess Enjoyer'],
+    strings: ['Finance Student', 'Data & Analytics Enthusiast', 'Web Designer', 'Aspiring Analyst'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
